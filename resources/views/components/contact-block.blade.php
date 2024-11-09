b3f4f93ed636a97c8eb274a69ec9bc8b5ee7e624
Petro Chaikivskyi <pchaikivskyi@gmail.com>
2024-11-10 01:37:27 +0200

    update contact form styles

--- resources/views/components/contact-block.blade.php
+++ resources/views/components/contact-block.blade.php
@@ -1,11 +1,14 @@
-<div>
-    <div class="w-full border-b-2 my-5 px-5 lg:p-0">
+<div class="px-5 lg:p-0">
+    <div class="w-full border-b-2 my-5">
         <h2 id="contact-me-header" class="text-4xl font-extrabold mb-2">Contact Me</h2>
     </div>
 
-    <div class="sm:flex sm:flex-row sm:justify-center mx-5">
-        <livewire:contact-me.contact-form />
-        <div class="hidden sm:block border-l-2 mx-10"></div>
-        <livewire:contact-me.contact-info />
+    <div class="sm:grid sm:grid-cols-2">
+        <div class="sm:border-r-2 sm:ml-auto sm:pr-10 w-full sm:max-w-sm">
+            <livewire:contact-me.contact-form />
+        </div>
+        <div class="sm:ml-10">
+            <livewire:contact-me.contact-info />
+        </div>
     </div>
 </div>
--- resources/views/livewire/contact-me/contact-form.blade.php
+++ resources/views/livewire/contact-me/contact-form.blade.php
@@ -1,5 +1,5 @@
 <div class="mb-5">
-    <form class="max-w-sm" wire:submit.prevent="submit">
+    <form wire:submit.prevent="submit">
         <div class="mb-6">
             <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Your name</label>
             <input wire:model="name" type="text" id="name" class="shadow-sm bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 @error('name') border-solid border-1 border-red-800 @else border-gray-300 @enderror" placeholder="John Smith" required />
@@ -21,7 +21,7 @@
                 @error('message') <span class="absolute text-red-800">{{ $message }}</span> @enderror
             </div>
         </div>
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Send Message</button>
+        <button type="submit" class="w-full sm:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Send Message</button>
     </form>
 </div>
 
