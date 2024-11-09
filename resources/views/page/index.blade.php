43e9bc00636176f7999ea6241a52db8e62084974
Petro Chaikivskyi <pchaikivskyi@gmail.com>
2024-11-10 01:06:50 +0200

    Add max height to banners on home page

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layout.one-column')
 
 @section('content')
-    <img class="hidden lg:block" src="{{ URL::asset('media/home/main.jpeg') }}" />
-    <img class="lg:hidden" src="{{ URL::asset('media/home/main-mobile.jpg') }}" />
+    <img class="hidden lg:block w-full max-h-[550px] object-cover" src="{{ URL::asset('media/home/main.jpeg') }}" />
+    <img class="lg:hidden w-full max-h-124 object-cover" src="{{ URL::asset('media/home/main-mobile.jpg') }}" />
 
     <livewire:spotify.spotify-tracks />
     @include('components.contact-block')
