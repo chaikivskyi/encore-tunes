a53b65b2a5a23b37ae23165a67c6e1a2c891122d
Petro Chaikivskyi <pchaikivskyi@gmail.com>
2024-12-24 15:02:47 +0200

    Allow create avaibility request only authentificated users

--- app/Livewire/RequestAvailability/RequestAvailabilityForm.php
+++ app/Livewire/RequestAvailability/RequestAvailabilityForm.php
@@ -30,17 +30,15 @@ class RequestAvailabilityForm extends Component
 
     public function submit(AvailabilityRequestRepository $repository)
     {
+        if (! Auth::check()) {
+            abort(403);
+        }
+
         try {
             $data = $this->validate();
             $lock = Cache::lock('availability_request', 10);
 
             if ($lock->block(5)) {
-                $requests = $repository->getActiveRequests();
-
-                if ($this->hasDateConflict($data, $requests)) {
-                    throw ValidationException::withMessages(['date_from' => 'Selected date is already booked.']);
-                }
-
                 $userId = Auth::id();
                 $data['user_id'] = $userId;
                 $request = $repository->create($data);
@@ -59,7 +57,7 @@ class RequestAvailabilityForm extends Component
         } catch (LockTimeoutException) {
             $this->addErrorMessage('System is busy. Please try again later in a few minutes.');
         } catch (Throwable $e) {
-            $this->addErrorMessage('Something went wrong. Please try again later.' . $e->getMessage());
+            $this->addErrorMessage('Something went wrong. Please try again later.');
         } finally {
             $lock->release();
         }
@@ -68,19 +66,5 @@ class RequestAvailabilityForm extends Component
     public function render()
     {
         return view('livewire.request-availability.request-availability-form');
-    }
-
-    protected function hasDateConflict(array $data, $requests): bool
-    {
-        foreach ($requests as $request) {
-            if (
-                ($data['date_from'] >= $request->date_from && $data['date_from'] <= $request->date_to)
-                || (!empty($data['date_to']) && $data['date_to'] >= $request->date_from && $data['date_to'] <= $request->date_to)
-            ) {
-                return true;
-            }
-        }
-
-        return false;
     }
 }
--- resources/views/livewire/request-availability/request-availability-form.blade.php
+++ resources/views/livewire/request-availability/request-availability-form.blade.php
@@ -1,33 +1,41 @@
-<form x-data="{
+<div>
+    @auth()
+        <form x-data="{
 beforeSubmit: function () {
     $wire.set('date_from', $store.requestAvailabilityDateStart);
     $wire.set('date_to', $store.requestAvailabilityDateEnd);
 }}" wire:submit="submit" x-on:submit.prevent="beforeSubmit">
-    <div>
-        <x-input-label for="contact-data-comment" :value="__('Contact Data')"/>
-        <x-text-input id="contact-data-comment" type="text" wire:model="contact_data" name="contact_data" autofocus
-                      placeholder="Email, Phone, Instagram, etc..."/>
-        <x-input-error :messages="$errors->get('contact_data')" class="mt-2"/>
-    </div>
+            <div>
+                <x-input-label for="contact-data-comment" :value="__('Contact Data')"/>
+                <x-text-input id="contact-data-comment" type="text" wire:model="contact_data" name="contact_data" autofocus
+                              placeholder="Email, Phone, Instagram, etc..."/>
+                <x-input-error :messages="$errors->get('contact_data')" class="mt-2"/>
+            </div>
 
-    <div class="mt-4">
-        <x-input-label for="request-availability-comment" :value="__('Comment')"/>
-        <x-textarea id="request-availability-comment" type="textbox" wire:model="comment" name="comment" autofocus/>
-        <x-input-error :messages="$errors->get('comment')" class="mt-2"/>
-    </div>
+            <div class="mt-4">
+                <x-input-label for="request-availability-comment" :value="__('Comment')"/>
+                <x-textarea id="request-availability-comment" type="textbox" wire:model="comment" name="comment" autofocus/>
+                <x-input-error :messages="$errors->get('comment')" class="mt-2"/>
+            </div>
 
-    <div class="mt-4 text-right">
-        <x-primary-button>
-            {{ __('Submit') }}
-        </x-primary-button>
-    </div>
-</form>
+            <div class="mt-4 text-right">
+                <x-primary-button>
+                    {{ __('Submit') }}
+                </x-primary-button>
+            </div>
+        </form>
+    @endauth
 
-@push('scripts')
-    <script>
-        document.addEventListener('alpine:init', () => {
-            Alpine.store('requestAvailabilityDateStart');
-            Alpine.store('requestAvailabilityDateEnd');
-        });
-    </script>
-@endpush
+    @guest
+        <div class="text-lg text-yellow-500">Please login to be able to request availability</div>
+    @endguest
+
+    @push('scripts')
+        <script>
+            document.addEventListener('alpine:init', () => {
+                Alpine.store('requestAvailabilityDateStart');
+                Alpine.store('requestAvailabilityDateEnd');
+            });
+        </script>
+    @endpush
+</div>
